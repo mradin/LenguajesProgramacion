36f62fab9d9d3a2f187d25a8e67c73537da565f1
mradin <mradin@gmail.com>
2016-08-16 10:27:50 -0500

    Ejercicio 1
    1st

---
+++ Ejercicio1/ejercicios.hs
@@ -0,0 +1,31 @@
+fact 1 = 1
+fact x = x * fact(x-1)
+maximum [] = -1
+maximum list = max (tail list) (head list)
+			   where max list current
+					 | (length list) == 0 = current
+					 | (head list) > current = max (tail list)(head list)
+					 | otherwise = max(tail list) current
+					 
+bigger (a,b) = if  a > b 
+			   then a 
+			   else b
+
+smaller (a,b) = if  a < b 
+				then a 
+				else b
+maximo [] = -1
+maximo (a:[]) = a
+maximo (a:b:c) = if c == []
+				 then bigger(a,b)
+				 else maximo([bigger(a,b)]++c)
+
+minimo [] = -1
+minimo (a:[]) = a
+minimo (a:b:c) = if c == []
+				 then smaller(a,b)
+				 else maximo([minimo(a,b)]++c)
+				 			   
+maxmin [] = -1
+maxmin (a:[]) = ()
+maxmin lista = (minimo lista maximo lista)
